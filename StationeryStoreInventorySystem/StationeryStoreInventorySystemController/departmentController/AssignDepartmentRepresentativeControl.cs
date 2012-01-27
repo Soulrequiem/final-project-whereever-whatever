51f35e83446063550d8158316dad81cdbc964cea
sanlapyaye <sanlapyaye@gmail.com>
2012-01-27 09:48:57 +0000

    commit after boss committed.

--- StationeryStoreInventorySystem/StationeryStoreInventorySystemController/departmentController/AssignDepartmentRepresentativeControl.cs
+++ StationeryStoreInventorySystem/StationeryStoreInventorySystemController/departmentController/AssignDepartmentRepresentativeControl.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿/***************************************************************************/
+/*  File Name       : AssignDepartmentRepresentativeControl.cs
+/*  Module Name     : Controller
+/*  Owner           : SanLaPyaye
+/*  class Name      : AssignDepartmentRepresentativeControl
+/*  Details         : Controller representation of AssignDepartmentRepresentativeControl
+/***************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using StationeryStoreInventorySystemModel.brokerinterface;
 using StationeryStoreInventorySystemModel.broker;
 using StationeryStoreInventorySystemModel.entity;
-using StationeryStoreInventorySystemController.commonController;
 using SystemStoreInventorySystemUtil;
 using System.Data;
 
@@ -17,12 +23,23 @@ namespace StationeryStoreInventorySystemController.departmentController
         private Employee currentEmployee;
         private Employee departmentRepresentative;
         private Employee temporaryDepartmentRepresentative;
-        
+        /// <summary>
+        ///     The usage of this method to call GetEmployee() for show the Current Department Representative
+        ///     Created By: SanLaPyaye
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+
         public AssignDepartmentRepresentativeControl()
         {
             currentEmployee = Util.ValidateUser(Constants.EMPLOYEE_ROLE.DEPARTMENT_HEAD);
             departmentRepresentative = currentEmployee.Department.Employee2;
-            
+
             employeeBroker = new EmployeeBroker();
 
             temporaryDepartmentRepresentative = new Employee();
@@ -35,7 +52,7 @@ namespace StationeryStoreInventorySystemController.departmentController
 
         public DataTable DepartmentRepresentative
         {
-            get 
+            get
             {
                 DataTable dt = new DataTable();
                 DataRow dr;
@@ -53,7 +70,7 @@ namespace StationeryStoreInventorySystemController.departmentController
 
                 if (temporaryDepartmentRepresentative != null)
                 {
-                    dr  = new DataRow();
+                    dr = new DataRow();
 
                     dt.NewRow();
                     dr["representativeId"] = temporaryDepartmentRepresentative.Id;
@@ -62,43 +79,24 @@ namespace StationeryStoreInventorySystemController.departmentController
                     dt.Rows.Add(dr);
                 }
 
-                return dt; 
+                return dt;
             }
         }
 
-        //public Employee TemporaryRepresentative
-        //{
-        //    get { return temporaryRepresentative; }
-        //}
 
-        //public Employee GetRepresentative()
-        //{
-        //    List<Employee> employeeList = employeeBroker.GetAllEmployee();
-        //    Employee e = null;
-        //    foreach(Employee employee in employeeList){
-        //        if(employee.Role.Name.Equals("Representative")){
-        //            e = employee;
-        //            break;
-        //        }
-        //    }
-        //    return e;
-        // }
-
-        //public Employee GetEmployee(Constants.EMPLOYEE_ROLE employeeRole)
-        //{
-        //    List<Employee> employeeList = employeeBroker.GetAllEmployee();
-        //    Employee e = null;
-        //    foreach (Employee employee in employeeList)
-        //    {
-        //        if (employee.Role.Name.Equals("Representative"))
-        //        {
-        //            e = employee;
-        //            break;
-        //        }
-        //    }
-        //    return e;
-        //}
-
+        /// <summary>
+        ///     The usage of this method is to remove the selected Actual Department Representative
+        ///     Created By: SanLaPyaye 
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+        /// <param name="employeeID"> Selected Employee ID from the UI</param>
+        /// <returns>Return the status of remove  whether Successful or Fail. </returns>
         public Constants.ACTION_STATUS SelectRemove(int employeeId)
         {
             Constants.ACTION_STATUS status = Constants.ACTION_STATUS.UNKNOWN;
@@ -110,6 +108,19 @@ namespace StationeryStoreInventorySystemController.departmentController
             return status;
         }
 
+        /// <summary>
+        ///     The usage of this method is to search the Employee by EmployeeName
+        ///     Created By: SanLaPyaye 
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+        /// <parm>employee Name to search the Employee</parm>
+        /// <returns>Return the Data Table of Employee  </returns>
         public DataTable SelectEmployeeName(string employeeName)
         {
             Employee emp = new Employee();
@@ -131,10 +142,22 @@ namespace StationeryStoreInventorySystemController.departmentController
             return dt;
         }
 
+        /// <summary>
+        ///     The usage of this method is to assign the Department Representative from Search Result
+        ///     Created By: SanLaPyaye 
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+        /// <returns>Return the status of assign  whether Successful or Fail. </returns>
         public Constants.ACTION_STATUS SelectAssign(int employeeId)
         {
             Constants.ACTION_STATUS status = Constants.ACTION_STATUS.UNKNOWN;
-            
+
             if (Util.Assign(employeeBroker, employeeId, Constants.EMPLOYEE_ROLE.DEPARTMENT_REPRESENTATIVE) == Constants.DB_STATUS.SUCCESSFULL)
                 status = Constants.ACTION_STATUS.SUCCESS;
             else
@@ -144,3 +167,7 @@ namespace StationeryStoreInventorySystemController.departmentController
         }
     }
 }
+
+/****************************************/
+/********* End of the Class *****************/
+/****************************************/
