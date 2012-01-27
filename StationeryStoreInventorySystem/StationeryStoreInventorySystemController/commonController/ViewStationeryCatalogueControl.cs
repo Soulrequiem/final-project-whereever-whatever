0f62b480c9d0f98aff1c7e431a2723594fefdda4
sanlapyaye <sanlapyaye@gmail.com>
2012-01-27 09:38:27 +0000

    commit after boss committed.

--- StationeryStoreInventorySystem/StationeryStoreInventorySystemController/commonController/ViewStationeryCatalogueControl.cs
+++ StationeryStoreInventorySystem/StationeryStoreInventorySystemController/commonController/ViewStationeryCatalogueControl.cs
@@ -1,34 +1,57 @@
-﻿using System;
+﻿/***************************************************************************/
+/*  File Name       : ViewStationeryCatalogueControl.cs
+/*  Module Name     : Controller
+/*  Owner           : SanLaPyaye
+/*  class Name      : ViewStationeryCatalogueControl
+/*  Details         : Controller representation ViewStationeryCatalogueControl 
+/***************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using StationeryStoreInventorySystemModel.brokerinterface;
 using StationeryStoreInventorySystemModel.broker;
 using StationeryStoreInventorySystemModel.entity;
 using SystemStoreInventorySystemUtil;
 using System.Data;
 
+
 namespace StationeryStoreInventorySystemController.commonController
 {
-    class ViewStationeryCatalogueControl
+    public class ViewStationeryCatalogueControl
     {
+
+        ///     The usage of this method is to call GetAllItems() for show the all stationery catalogue
+        ///     Created By: SanLaPyaye 
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+
         IItemBroker itemBroker;
         Employee currentEmployee;
-        
+
         public ViewStationeryCatalogueControl()
         {
             currentEmployee = Util.ValidateUser();
             itemBroker = new ItemBroker();
         }
 
-        /// <summary>
-        ///     Method to return stationery items according to role
-        ///     Created By: Taufin Rusli
-        ///     Created Date: 2012/01/27
+        ///     The usage of this method is to show the all stationery catalogue
+        ///     Created By: SanLaPyaye
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
         /// </summary>
-        /// <returns>
-        ///     Datatable of all the stationery items
-        /// </returns>
+        /// <returns>Return the list of stationery catalogue. </returns>
         public DataTable GetAllItem()
         {
             DataTable dt = new DataTable();
@@ -69,9 +92,41 @@ namespace StationeryStoreInventorySystemController.commonController
             return dt;
         }
 
+
+        ///     The usage of this method is to assign the selected Department Head from Search Result
+        ///     Created By: SanLaPyaye
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+        /// <param name="remarks"> Remark to update the employee.</param>
+        /// <returns>Return the status of assign  whether Successful or Fail. </returns>
+        //public Item SelectItemDescription(string itemDescription)
+        //{            
+        //    itembroker = new ItemBroker();          //Boss will do the google search 
+        //    Item item= new Item();
+        //    item.Description = itemDescription;
+        //   Item i= itembroker.GetItem(item);
+        //   return i;
+        //}
         public Item SelectItemDescription(string itemDescription)
         {
             return Util.GetItem(itemDescription);
         }
+
+        //public Item GetItem(Item item)
+        //{
+        //    ItemBroker itembroker = new ItemBroker();
+        //    return itembroker.GetItem(item);
+        //}
+
+
     }
 }
+/****************************************/
+/********* End of the Class *****************/
+/****************************************/
