798d8528258366bf5ce6003152f3a05d943e9023
sanlapyaye <sanlapyaye@gmail.com>
2012-01-27 03:26:37 +0000

    ApproveRejectRequisitionControl committed by SLP

--- StationeryStoreInventorySystem/StationeryStoreInventorySystemController/departmentController/ApproveRejectRequisitionControl.cs
+++ StationeryStoreInventorySystem/StationeryStoreInventorySystemController/departmentController/ApproveRejectRequisitionControl.cs
@@ -1,58 +1,147 @@
-﻿using System;
+﻿/***************************************************************************/
+/*  File Name       : ApproveRejectRequisitionControl.cs
+/*  Module Name     : Controller
+/*  Owner           : SanLaPyaye
+/*  class Name      : ApproveRejectRequisitionControl
+/*  Details         : Controller representation of ApproveRejectRequisitionControl
+/***************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using StationeryStoreInventorySystemModel.brokerinterface;
+using SystemStoreInventorySystemUtil;
 using StationeryStoreInventorySystemModel.broker;
 using StationeryStoreInventorySystemModel.entity;
-using StationeryStoreInventorySystemController.commonController;
-using SystemStoreInventorySystemUtil;
+using System.Data;
 
 namespace StationeryStoreInventorySystemController.departmentController
 {
-    public class ApproveRejectRequisitionControl
+    class ApproveRejectRequisitionControl
     {
-        IRequisitionBroker requisitionBroker = new RequisitionBroker();
-        public ApproveRejectRequisitionControl()
+        commonController.RequisitionDetailsControl requisitionDetailsControl;
+        RequisitionBroker requisitionBroker = new RequisitionBroker();
+        DataTable dt;
+        DataRow dr;
+
+
+        /// <summary>
+        ///     The usage of this method is to show the Requisitions List.
+        ///     Created By: SanLaPyaye
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+
+        public ApproveRejectRequisitionControl() 
         {
+             requisitionDetailsControl = new commonController.RequisitionDetailsControl();
+                     
         }
 
-        public List<Requisition> GetAllRequisitions(){
-            
+        /// <summary>
+        ///     The usage of this method is to show the requisitions list  
+        ///     Created By: SanLaPyaye 
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+        /// <returns>Return the list of requisitions. </returns>
+
+        //***********
+        //public List<Requisition> GetRequisition()
+        //{            
+        //    return requisitionBroker.GetAllRequisition();
+        //}
+        //***********
+
+        public DataTable GetRequisition()
+        {
+            dt = new DataTable();
+            dr = null;
+
             List<Requisition> requisitionList = requisitionBroker.GetAllRequisition();
 
-            return requisitionList;
+            int qty;
+            foreach (Requisition r in requisitionList)
+            {
+                qty=0;
+                dt.NewRow();
+                dr = new DataRow();
+                dr["requisitionId"] = r.Id;
+                dr["requisitionDate/Time"] = Convert.ToDateTime(r.CreatedDate);
+                dr["requisitionBy"] = r.Employee.Name;
+
+                
+            //List<RequisitionDetail> requisitionDetailList=requisitionBroker.GetRequisitionDetail(r.RequisitionDetails);
+            foreach(RequisitionDetail reqDetail in r.RequisitionDetails.ToList())
+            {
+               qty += reqDetail.Qty;
+            }
+                dr["requiredQty"] = qty;
+                dr["remarks"] = r.Remarks;
+
+                dt.Rows.Add(dr);
+
+            }
+
+            return dt;
         }
 
-        public void SelectRequisitionID(String requisitionID)
+        /// <summary>
+        ///     The usage of this method is to show the "Requisition Details"
+        ///     Created By: SanLaPyaye 
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+        /// <param name="requisitionID">Get the "Requisition ID" when user click "Requisition ID" link. </param>
+        public void SelectRequisitionID(string requisitionID)
         {
-            Requisition requisition = new Requisition();
+            Requisition requisition=new Requisition();
             requisition.Id = requisitionID;
-            RequisitionDetailsControl requisitionDetailsControl = new RequisitionDetailsControl();
+            requisitionDetailsControl=new commonController.RequisitionDetailsControl();
             requisitionDetailsControl.SelectRequisitionID(requisition);
         }
 
-        public Constants.ACTION_STATUS SelectApproveRequisition(Requisition requisition)
+
+        /// <summary>
+        ///     The usage of this method is to approve the selected "Requisition ID" 
+        ///     Created By: SanLaPyaye 
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+        /// <param name="requisitionID">Get the "Requisition ID" when user click "Requisition ID" link. </param>
+        public Constants.DB_STATUS SelectApproveRequisition(string requisitionID)
         {
-            Constants.ACTION_STATUS status = Constants.ACTION_STATUS.UNKNOWN;
-            requisition.Status = (int) Constants.REQUISITION_STATUS.APPROVED;
+            Constants.DB_STATUS status = Constants.DB_STATUS.UNKNOWN;
+            Requisition requisition = new Requisition();
+            requisition.Id = requisitionID;
             requisitionBroker.Update(requisition);
-            status = Constants.ACTION_STATUS.SUCCESS;
             return status;
+            
         }
-
-        public Constants.ACTION_STATUS SelectRejectRequisition(Requisition requisition)
-        {
-            Constants.ACTION_STATUS status = Constants.ACTION_STATUS.UNKNOWN;
-            requisition.Status = (int)Constants.REQUISITION_STATUS.REJECTED;
-            Constants.DB_STATUS dbStatus = requisitionBroker.Update(requisition);
-            if (dbStatus == Constants.DB_STATUS.SUCCESSFULL)
-                status = Constants.ACTION_STATUS.SUCCESS;
-            else
-                status = Constants.ACTION_STATUS.FAIL;
-            return status;
-
-        }
-
+       
+       
+      
     }
 }
+/****************************************/
+/********* End of the Class *****************/
+/****************************************/
