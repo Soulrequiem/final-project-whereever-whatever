134ec40a3f0b97f46547718664bd398840e58b60
sanlapyaye@gmail.com <sanlapyaye@gmail.com@d3ed2185-4dd4-47dd-262c-80c6c1db147c>
2012-01-27 03:25:37 +0000

    ViewStationeryCatalogueControl comitted by SLP

--- trunk/StationeryStoreInventorySystem/StationeryStoreInventorySystemController/commonController/ViewStationeryCatalogueControl.cs
+++ trunk/StationeryStoreInventorySystem/StationeryStoreInventorySystemController/commonController/ViewStationeryCatalogueControl.cs
@@ -1,50 +1,117 @@
-﻿using System;
+﻿/***************************************************************************/
+/*  File Name       : ViewStationeryCatalogueControl.cs
+/*  Module Name     : Controller
+/*  Owner           : SanLaPyaye
+/*  class Name      : ViewStationeryCatalogueControl
+/*  Details         : Controller representation ViewStationeryCatalogueControl 
+/***************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using StationeryStoreInventorySystemModel.brokerinterface;
 using StationeryStoreInventorySystemModel.broker;
 using StationeryStoreInventorySystemModel.entity;
 using SystemStoreInventorySystemUtil;
 using System.Data;
 
+
 namespace StationeryStoreInventorySystemController.commonController
 {
     class ViewStationeryCatalogueControl
     {
-        IItemBroker itemBroker;
-        Employee employee;
+        ItemBroker itembroker;
+        DataTable dt;
+        DataRow dr;
+
         
-        public ViewStationeryCatalogueControl()
+        ///     The usage of this method is to call GetAllItems() for show the all stationery catalogue
+        ///     Created By: SanLaPyaye 
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+        public ViewStationeryCatalogueControl() 
         {
-            employee = Util.ValidateUser();
-            itemBroker = new ItemBroker();
+            
         }
 
-        public DataTable GetAllItem()
+        ///     The usage of this method is to show the all stationery catalogue
+        ///     Created By: SanLaPyaye
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+        /// <returns>Return the list of stationery catalogue. </returns>
+        public DataTable GetAllItems()
         {
-            DataTable dt = new DataTable();
-            DataRow dr;
-            List<Item> itemList = itemBroker.GetAllItem();
-
-            foreach (Item item in itemList)
+            dt = new DataTable();
+            dr = null;
+                        
+            itembroker = new ItemBroker();
+            List<Item> itemList= itembroker.GetAllItem();
+            foreach (Item i in itemList)
             {
-                // only for employee, need to add the one for clerk
                 dt.NewRow();
                 dr = new DataRow();
-                dr["itemNo"] = item.Id;
-                dr["category"] = Converter.GetItemCategoryText(Converter.objToItemCategory(item.ItemCategoryId));
-                dr["itemDescription"] = item.Description;
-                dr["unitOfMeasure"] = Converter.GetUnitOfMeasureText(Converter.objToUnitOfMeasure(item.UnitOfMeasureId));
+
+                dr["itemNo"] = i.Id;
+                dr["category"] =Converter.GetItemCategoryText(Converter.objToItemCategory(i.ItemCategoryId));
+                dr["itemDescription"] = i.Description;
+                dr["reorderLevel"] = i.ReorderLevel;
+                dr["reorderQty"] = i.ReorderQty;
+                dr["unitOfMeasure"] =Converter.GetUnitOfMeasureText(Converter.objToUnitOfMeasure(i.UnitOfMeasureId));
                 dt.Rows.Add(dr);
             }
 
             return dt;
         }
 
-        public List<Item> SelectItemDescription(string itemDescription)
-        {
-            return Util.GetItems(itemDescription);
+       
+        ///     The usage of this method is to assign the selected Department Head from Search Result
+        ///     Created By: SanLaPyaye
+        ///     Created Date: 25/01/2012
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        ///     Modified By:
+        ///     Modified Date:
+        ///     Modification Reason:
+        /// </summary>
+        /// <param name="remarks"> Remark to update the employee.</param>
+        /// <returns>Return the status of assign  whether Successful or Fail. </returns>
+        public Item SelectItemDescription(string itemDescription)
+        {            
+            itembroker = new ItemBroker();          //Boss will do the google search 
+            Item item= new Item();
+            item.Description = itemDescription;
+           Item i= itembroker.GetItem(item);
+           return i;
         }
+        
+        // The method which created by ZMT
+        //public List<Item> SelectItemDescription(string itemDescription)
+        //{
+        //    return Util.GetItems(itemDescription);
+        //}
+
+        //public Item GetItem(Item item)
+        //{
+        //    ItemBroker itembroker = new ItemBroker();
+        //    return itembroker.GetItem(item);
+        //}
+
+      
     }
 }
+/****************************************/
+/********* End of the Class *****************/
+/****************************************/
