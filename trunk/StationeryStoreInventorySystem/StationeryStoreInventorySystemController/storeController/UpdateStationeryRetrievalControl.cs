2cc90030f6596feb2ec298d878df1bc52a67ff5a
taufinrusli@gmail.com <taufinrusli@gmail.com@d3ed2185-4dd4-47dd-262c-80c6c1db147c>
2012-01-28 06:31:00 +0000

    add for boss cc

---
+++ trunk/StationeryStoreInventorySystem/StationeryStoreInventorySystemController/storeController/UpdateStationeryRetrievalControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SystemStoreInventorySystemUtil;
+
+namespace StationeryStoreInventorySystemController.storeController
+{
+    public class UpdateStationeryRetrievalControl
+    {
+        public Constants.ACTION_STATUS SetCollectionStatus(Constants.COLLECTION_STATUS collectionStatus, List<string> collectionIdList)
+        {
+        }
+    }
+}
