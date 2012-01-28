290e0053ae2c312cd88940384cf4cc326d67c694
waiyaniad <waiyaniad@gmail.com>
2012-01-28 09:28:26 +0000

    Common validation

--- StationeryStoreInventorySystem/StationeryStoreInventorySystem/commonUI/ForgotPassword.aspx.designer.cs
+++ StationeryStoreInventorySystem/StationeryStoreInventorySystem/commonUI/ForgotPassword.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace SA34_Team9_StationeryStoreInventorySystem.commonUI {
         protected global::Infragistics.Web.UI.EditorControls.WebTextEditor txtemailaddress;
         
         /// <summary>
+        /// lblStatusMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblStatusMessage;
+        
+        /// <summary>
         /// btnSend control.
         /// </summary>
         /// <remarks>
