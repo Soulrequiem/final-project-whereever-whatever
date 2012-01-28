6fd5a401d16a93d2c65f20eac29082c58afa4a66
waiyaniad <waiyaniad@gmail.com>
2012-01-28 09:27:52 +0000

    Representative validation

--- StationeryStoreInventorySystem/StationeryStoreInventorySystem/departmentUI/Representative/ManageCollectionPoint.aspx.designer.cs
+++ StationeryStoreInventorySystem/StationeryStoreInventorySystem/departmentUI/Representative/ManageCollectionPoint.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace SA34_Team9_StationeryStoreInventorySystem.departmentUI.Representative 
         protected global::Infragistics.Web.UI.ListControls.WebDropDown drdCollectionList;
         
         /// <summary>
+        /// lblStatusMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblStatusMessage;
+        
+        /// <summary>
         /// btnSave control.
         /// </summary>
         /// <remarks>
