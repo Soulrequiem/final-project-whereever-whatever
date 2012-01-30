495d8ba7bad654e619a5aceb4502481f5f4c5316
taufinrusli@gmail.com <taufinrusli@gmail.com@d3ed2185-4dd4-47dd-262c-80c6c1db147c>
2012-01-30 02:42:51 +0000

    for thazin~

---
+++ trunk/StationeryStoreInventorySystem/StationeryStoreInventorySystemModel/broker/CollectionMissedBroker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StationeryStoreInventorySystemModel.broker
+{
+    public class CollectionMissedBroker : StationeryStoreInventorySystemModel.brokerinterface.ICollectionMissedBroker
+    {
+
+        public entity.CollectionMissed GetCollectionMissed(entity.CollectionMissed collectionMissed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<entity.CollectionMissed> GetAllCollectionMissed(entity.Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<entity.CollectionMissed> GetAllCollectionMissed(entity.Department department, SystemStoreInventorySystemUtil.Constants.VISIBILITY_STATUS status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SystemStoreInventorySystemUtil.Constants.DB_STATUS Insert(entity.CollectionMissed newCollectionMissed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SystemStoreInventorySystemUtil.Constants.DB_STATUS Update(entity.CollectionMissed collectionMissed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SystemStoreInventorySystemUtil.Constants.DB_STATUS Delete(entity.CollectionMissed collectionMissed)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/StationeryStoreInventorySystem/StationeryStoreInventorySystemModel/brokerinterface/INotificationBroker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StationeryStoreInventorySystemModel.entity;
+using SystemStoreInventorySystemUtil;
+
+namespace StationeryStoreInventorySystemModel.brokerinterface
+{
+    public interface INotificationBroker
+    {
+        Notification GetNotification(Notification notification);
+        List<Notification> GetAllNotification();
+        List<Notification> GetAllNotification(Constants.NOTIFICATION_STATUS notificationStatus);
+        Constants.DB_STATUS Insert(Notification newNotification);
+        Constants.DB_STATUS Insert(List<Notification> newNotificationList);
+        Constants.DB_STATUS Update(Notification notification);
+        Constants.DB_STATUS Delete(Notification notification);
+    }
+}
