eac8c2ca7836dc27af0feefb5bc3f61f54eb8370
jinchengcheng20102011 <jinchengcheng20102011@gmail.com>
2012-02-02 07:44:36 +0000

    done by taufin

--- StationeryStoreInventorySystem/StationeryStoreInventorySystem/commonUI/ViewStationeryCatalogue.aspx.designer.cs
+++ StationeryStoreInventorySystem/StationeryStoreInventorySystem/commonUI/ViewStationeryCatalogue.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace SA34_Team9_StationeryStoreInventorySystem.commonUI {
         protected global::Infragistics.Web.UI.ListControls.WebDropDown drdItemList;
         
         /// <summary>
+        /// btnSearch control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSearch;
+        
+        /// <summary>
         /// Label5 control.
         /// </summary>
         /// <remarks>
