97a25c298f1c05cdf91614169f6f35ec53686a02
jinchengcheng20102011 <jinchengcheng20102011@gmail.com>
2012-02-02 07:44:12 +0000

    done by taufin

--- StationeryStoreInventorySystem/StationeryStoreInventorySystemController/Util.cs
+++ StationeryStoreInventorySystem/StationeryStoreInventorySystemController/Util.cs
@@ -194,6 +194,9 @@ namespace StationeryStoreInventorySystemController
             return item;
         }
 
+        //CC
+       
+
         public static DataTable GetItemTable(IItemBroker itemBroker, string itemDescription)
         {
             Item item = GetItem(itemBroker, itemDescription);
@@ -214,37 +217,9 @@ namespace StationeryStoreInventorySystemController
             return dt;
         }
 
-        public static DataTable GetItemListTable(IItemBroker itemBroker, string itemDescription)
+        public static DataTable GetItemListTable( string itemDescription)
         {
-            DataTable dt = new DataTable();
-            dt.Columns.AddRange(new DataColumn[] { new DataColumn("ItemNo"), 
-                                                   new DataColumn("Category"), 
-                                                   new DataColumn("ItemDescription"),
-                                                   new DataColumn("ReorderLevel"),
-                                                   new DataColumn("ReorderQty"),
-                                                   new DataColumn("UnitOfMeasure") });
-            DataRow dr;
-
-            Item it = new Item();
-            it.Description = itemDescription;
-            it = itemBroker.GetItem(it);
-
-            List<Item> itemList = (new ItemBroker(new InventoryEntities())).GetItemReference(it);
-
-            foreach (Item item in itemList)
-            {
-                dr = dt.NewRow();
-                dr["ItemNo"] = item.Id;
-                dr["Category"] = Converter.GetItemCategoryText(Converter.objToItemCategory(item.ItemCategoryId));
-                dr["ItemDescription"] = item.Description;
-                dr["ReorderLevel"] = item.ReorderLevel;
-                dr["ReorderQty"] = item.ReorderQty;
-                dr["UnitOfMeasure"] = Converter.GetUnitOfMeasureText(Converter.objToUnitOfMeasure(item.UnitOfMeasureId));
-                dt.Rows.Add(dr);
-            }
-
-
-            return dt;
+            return GetItemTable().Select("ItemDescription LIKE '" + itemDescription + "%'").CopyToDataTable();
         }
 
         private static DataTable allItem;
